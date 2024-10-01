047d0b29050cb9378c29fdf0e45e7371f7397ab6
faustinopsy <rodrigohipnose@gmail.com>
2024-10-01 19:21:21 -0300

    alterando a lista para um array de objetos e um loop para criar item por item

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+const users =[
+    {"usuario_id":1,"nome":"AAA BB CC","email":"xxxxx@gmail.com"},
+    {"usuario_id":1,"nome":"LLL UUU OOO","email":"llllll@ri.com"},
+    {"usuario_id":1,"nome":"ZZZ OOO EEE","email":"zzzzz@gmail.com"},
+]
 function crieForm(){
     const form = document.createElement('form')
     form.id = "formUsers"
@@ -14,11 +19,11 @@ function crieForm(){
 function criaLista(){
     const lista = document.createElement('ul')
     lista.id = "listausers"
-    lista.innerHTML = `
-        <li>AAA BB CC ,xxxxx@gmail.com</li>
-        <li>LLL UUU OOO ,llllll@ri.com</li>
-        <li>ZZZ OOO EEE ,zzzzz@gmail.com</li>
-    ` 
+    users.forEach((usuario)=>{
+        const li = document.createElement('li')
+        li.innerHTML = `${usuario.nome} , ${usuario.email}`
+        lista.appendChild(li)
+    })
     return lista
 }
 
