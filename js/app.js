49d5202ec546c207b28c4c427794df67eb119a66
faustinopsy <rodrigohipnose@gmail.com>
2024-10-01 19:15:24 -0300

    alterado para as funções serem criadoras de elementos

--- js/app.js
+++ js/app.js
@@ -1,15 +1,28 @@
-document.getElementById("app").innerHTML = `
-    <form>
+function crieForm(){
+    const form = document.createElement('form')
+    form.id = "formUsers"
+    form.innerHTML = `
         <label for="nome">Nome</label>
         <input type="text" name="nome" id="nome">
         <label for="email">email</label>
         <input type="email" name="email" id="email">
         <label for="senha">senha</label>
         <input type="password" name="senha" id="senha">
-        <button>Enviar</button>
-    </form>
-    <ul>
+        <button>Enviar</button>`
+    return form
+}
+function criaLista(){
+    const lista = document.createElement('ul')
+    lista.id = "listausers"
+    lista.innerHTML = `
         <li>AAA BB CC ,xxxxx@gmail.com</li>
         <li>LLL UUU OOO ,llllll@ri.com</li>
         <li>ZZZ OOO EEE ,zzzzz@gmail.com</li>
-    </ul>`
+    ` 
+    return lista
+}
+
+const panel = document.createElement('div')
+panel.appendChild(crieForm())
+panel.appendChild(criaLista())
+document.getElementById("app").appendChild(panel);
