0d66bbb6b64d2e928d6da82ce559eac464c480bf
faustinopsy <rodrigohipnose@gmail.com>
2024-10-01 19:02:17 -0300

    levando a contrução do elemento para o javascript

---
+++ js/app.js
@@ -0,0 +1,15 @@
+document.getElementById("app").innerHTML = `
+    <form>
+        <label for="nome">Nome</label>
+        <input type="text" name="nome" id="nome">
+        <label for="email">email</label>
+        <input type="email" name="email" id="email">
+        <label for="senha">senha</label>
+        <input type="password" name="senha" id="senha">
+        <button>Enviar</button>
+    </form>
+    <ul>
+        <li>AAA BB CC ,xxxxx@gmail.com</li>
+        <li>LLL UUU OOO ,llllll@ri.com</li>
+        <li>ZZZ OOO EEE ,zzzzz@gmail.com</li>
+    </ul>`
